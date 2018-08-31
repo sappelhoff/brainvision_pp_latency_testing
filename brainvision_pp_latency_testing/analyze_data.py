936f05f837d22a80ed3fb16b6130d8a4e2b9bf3c
Stefan Appelhoff <stefan.appelhoff@mailbox.org>
2018-08-31 14:18:14 +0200

    df integrity checks

--- brainvision_pp_latency_testing/analyze_data.py
+++ brainvision_pp_latency_testing/analyze_data.py
@@ -62,6 +62,51 @@ def read_vmrk(vmrk_path, key1, key2):
     return df
 
 
+def assert_df_integrity(df):
+    """Assert that the df is in the format we want."""
+    # df should have two keys
+    keys = df.mdesc.unique()
+    if len(keys) != 2:
+        raise ValueError('df should contain exactly two keys.')
+    else:
+        key1, key2 = keys
+
+    # key1 needs to come first
+    if df.mdesc.tolist()[0] != key1:
+        raise ValueError('Key1 should be first in df. '
+                         'Re-read .vmrk with proper arguments.')
+
+    # ... always followed by a key2. Thus, last key should be key2
+    if df.mdesc.tolist()[-1] != key2:
+        raise ValueError('Key2 should be last in df. '
+                         'For every sent key1, there should be a key2 next. '
+                         'Perhaps, re-do collect_data.py')
+
+    # ... thus, length needs to be even
+    if len(df.mdesc) % 2 != 0:
+        raise ValueError('df should have an even length.')
+
+    # Assert ordering is key1, key2, key1, ... , key2
+    prev = df.mdesc[0]
+    for i in df.mdesc[1::]:
+        if prev == i:
+            raise ValueError('Keys 1 and 2 must always alternate')
+        prev = i
+
+    # Assert the onsets are all integers
+    for i in df.onset:
+        if not isinstance(i, int):
+            raise ValueError('All onsets must be integers')
+
+    # Assert the onsets are monotonously increasing
+    prev = df.onset[0]
+    for i in df.onset[1::]:
+        if prev >= i:
+            print(prev, i, prev >= i)
+            raise ValueError('Onsets must be monotonously increasing')
+        prev = i
+
+
 if __name__ == '__main__':
     warn('Assuming sampling frequency of {} Hz.'
          ' Make sure this is true!'.format(SAMPLING_FREQUENCY))
